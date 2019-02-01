ee5899f95f798885e95112293998d5209a863e18
Chad Tindel <chad@tindel.net>
2019-02-01 12:39:07 -0500

    API is working

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 STACK_NAME=ctindel-mb3
+export AWS_DEFAULT_REGION=us-east-2
 
 if ! aws cloudformation describe-stacks --stack-name $STACK_NAME > /dev/null 2>&1; then
     aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://`pwd`/mb3_cf.json --parameters file://`pwd`/params.json --capabilities CAPABILITY_IAM
--- undeploy.sh
+++ undeploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+export AWS_DEFAULT_REGION=us-east-2
+
 aws cloudformation delete-stack --stack-name ctindel-mb3
