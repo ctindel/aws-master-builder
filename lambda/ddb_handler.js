07897ed6fad46e688f0b68eaef981df94de67982
Chad Tindel <chad@tindel.net>
2019-02-20 20:33:17 +0000

    MDB Conversions are done

--- lambda/ddb_handler.js
+++ lambda/ddb_handler.js
@@ -1,36 +1,104 @@
 var aws = require("aws-sdk");
 const ddb = new aws.DynamoDB({apiVersion: '2012-08-10'});
+const util = require('util')
 
-function copyEvent(event, context, callback) {
-    console.log('Received event:', JSON.stringify(event, null, 2));
+const getTableNameFromARN = (arn) => {
+  /** This is a default dynamo table arn
+   *  arn:aws:dynamodb:eu-west-1:123456789012:table/table-name/stream/2015-11-13T09:23:17.104
+   *  After split
+   *  [
+   *    'arn:aws:dynamodb:eu-west-1:123456789012:table',
+   *    'table-name',
+   *    'stream',
+   *    '2015-11-13T09:23:17.104'
+   *  ]
+   */
+    return arn.split('/')[1].toLowerCase();
+}
 
-    event.Records.forEach(function(record) {
-        if (record.eventName == 'REMOVE') {
-            var key = record.dynamodb.OldImage.oid__id.S;
-            var params = { TableName: process.env.TARGET_DDB_TABLE, Key : { 'id' : { 'S' : record.dynamodb.OldImage.oid__id.S } } };
-            ddb.deleteItem(params, function(err, data) {
-                if (err) {
-                    console.error("copyEvent failed on ddb.deleteItem with key: " + key); 
-                    return callback(err);
-                }
-                console.log("Successfully processed delete for key " + key + " on table " + process.env.TARGET_DDB_TABLE);
-                return callback(null, "Successfully processed delete for key " + key + " on table " + process.env.TARGET_DDB_TABLE);
-            });
-        } else {
-            var newItem = record.dynamodb.NewImage;
-            newItem['id'] = { 'S' : record.dynamodb.NewImage.oid__id.S };
-            delete newItem.oid__id;
-            var params = { TableName: process.env.TARGET_DDB_TABLE, Item : newItem};
-            ddb.putItem(params, function(err, data) {
-                if (err) {
-                    console.error("copyEvent failed on ddb.putItem with key: " + newItem.id); 
-                    return callback(err);
-                }
-                console.log("Successfully processed update for key " + newItem.id.S + " on table " + process.env.TARGET_DDB_TABLE);
-                return callback(null, "Successfully processed update for key " + newItem.id.S + " on table " + process.env.TARGET_DDB_TABLE);
-            });
-        }
+function putProductItem(record, callback) {
+    var newItem = record.dynamodb.NewImage;
+    newItem['id'] = { 'S' : record.dynamodb.NewImage.oid__id.S };
+    delete newItem.oid__id;
+    var params = { TableName: process.env.TARGET_DDB_TABLE, Item : newItem};
+    ddb.putItem(params, function(err, data) {
+        return callback(err, data);
     });
 }
+
+function putReviewItem(record, callback) {
+    var newItem = record.dynamodb.NewImage;
+    newItem['id'] = { 'S' : record.dynamodb.NewImage.oid__id.S };
+    newItem['productId'] = { 'S' : record.dynamodb.NewImage.oid_productId.S };
+    delete newItem.oid__id;
+    delete newItem.oid_productId;
+    var params = { TableName: process.env.TARGET_DDB_TABLE, Item : newItem};
+    ddb.putItem(params, function(err, data) {
+        return callback(err, data);
+    });
+}
+
+function putOrderItem(record, callback) {
+    var newItem = record.dynamodb.NewImage;
+    newItem['id'] = { 'S' : record.dynamodb.NewImage.oid__id.S };
+    var newProductArray = [];
+    var products = JSON.parse(record.dynamodb.NewImage.array_products.S);
+    products.forEach(function(product) {
+        newProductArray.push({'M' : {'productId': { 'S' : product['productId']['$oid']}, 'count' : {'N' : product['count'].toString()}}});
+    });
+    newItem['products'] = { 'L' : newProductArray };
+    delete newItem.oid__id;
+    delete newItem.array_products;
+    var params = { TableName: process.env.TARGET_DDB_TABLE, Item : newItem};
+    ddb.putItem(params, function(err, data) {
+        return callback(err, data);
+    });
+}
+
+function putItem(record, callback) {
+    var tableName = getTableNameFromARN(record.eventSourceARN);
+
+    if (tableName == 'product') {
+        putProductItem(record, callback);
+    } else if (tableName == 'review') {
+        putReviewItem(record, callback);
+    } else if (tableName == 'order') {
+        putOrderItem(record, callback);
+    } else {
+        return callback("ERROR: unknown table name " + tableName, null);
+    }
+}
+
+function copyMDBItem(event, context, callback) {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+
+    if (event.Records.length !== 1) {
+        return callback("ERROR: This lambda function can only handle 1 event at a time", null);
+    }
+
+    var record = event.Records[0];
+    if (record.eventName == 'REMOVE') {
+        var key = record.dynamodb.OldImage.oid__id.S;
+        var params = { TableName: process.env.TARGET_DDB_TABLE, Key : { 'id' : { 'S' : record.dynamodb.OldImage.oid__id.S } } };
+        ddb.deleteItem(params, function(err, data) {
+            if (err) {
+                console.error("copyEvent failed on ddb.deleteItem with key: " + key); 
+                return callback(err);
+            }
+            console.log("Successfully processed delete for key " + key + " on table " + process.env.TARGET_DDB_TABLE);
+            return callback(null, "Successfully processed delete for key " + key + " on table " + process.env.TARGET_DDB_TABLE);
+        });
+    } else {
+        var key = record.dynamodb.NewImage.oid__id.S;
+        putItem(record, function(err, data) {
+            if (err) {
+                console.error("copyEvent failed on ddb.putItem with key: " + key);
+                return callback(err);
+            }
+            console.log("Successfully processed update for key " + key + " on table " + process.env.TARGET_DDB_TABLE);
+            return callback(null, "Successfully processed update for key " + key + " on table " + process.env.TARGET_DDB_TABLE);
+        });
+    }
+}
  
-exports.handler = copyEvent;
+exports.handler = copyMDBItem;
--- lambda/delete_ddb_mdb_items.sh
+++ lambda/delete_ddb_mdb_items.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
-export HASH_KEY="oid__id"
+export MDB_HASH_KEY="oid__id"
+export DDB_HASH_KEY="oid__id"
 
-declare -a tables=("product" "review" "order")
+declare -a MDB_TABLES=(
+    "product" 
+    "review" 
+    "order" 
+)
 
-for table in "${tables[@]}"
+declare -a DDB_TABLES=(
+    "ctindel-mb3-product" 
+    "ctindel-mb3-review" 
+    "ctindel-mb3-order"
+)
+
+for table in "${MDB_TABLES[@]}"
 do
     aws dynamodb scan \
-      --attributes-to-get $HASH_KEY \
+      --attributes-to-get $MDB_HASH_KEY \
+      --table-name $table --query "Items[*]" \
+      | jq --compact-output '.[]' \
+      | tr '\n' '\0' \
+      | xargs -0 -t -I keyItem \
+        aws dynamodb delete-item --table-name $table --key=keyItem
+done
+
+for table in "${DDB_TABLES[@]}"
+do
+    aws dynamodb scan \
+      --attributes-to-get $DDB_HASH_KEY \
       --table-name $table --query "Items[*]" \
       | jq --compact-output '.[]' \
       | tr '\n' '\0' \
